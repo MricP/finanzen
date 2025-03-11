79608adc381451be3a98e56403cf9aa1986e7936
Pirrera Emric <emric.pirrera@etu.univ-lyon1.fr>
2025-03-11 08:35:38 +0100

    url statistiques

--- migrations/Version20250311071946.php
+++ migrations/Version20250311071946.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250304152636 extends AbstractMigration
+final class Version20250311071946 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -26,16 +26,13 @@ final class Version20250304152636 extends AbstractMigration
         $this->addSql('CREATE TABLE liste_article (id INT AUTO_INCREMENT NOT NULL, articles_id INT NOT NULL, listes_id INT NOT NULL, est_achete TINYINT(1) NOT NULL, quantite INT NOT NULL, INDEX IDX_B30096371EBAF6CC (articles_id), INDEX IDX_B30096376F91B382 (listes_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE magasin (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(150) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE magasin_article (magasin_id INT NOT NULL, article_id INT NOT NULL, INDEX IDX_7585D73A20096AE3 (magasin_id), INDEX IDX_7585D73A7294869C (article_id), PRIMARY KEY(magasin_id, article_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, pseudo VARCHAR(100) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE utilisateur_liste (utilisateur_id INT NOT NULL, liste_id INT NOT NULL, INDEX IDX_3A37722AFB88E14F (utilisateur_id), INDEX IDX_3A37722AE85441D8 (liste_id), PRIMARY KEY(utilisateur_id, liste_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, pseudo VARCHAR(50) DEFAULT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E66BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
         $this->addSql('ALTER TABLE liste_article ADD CONSTRAINT FK_B30096371EBAF6CC FOREIGN KEY (articles_id) REFERENCES article (id)');
         $this->addSql('ALTER TABLE liste_article ADD CONSTRAINT FK_B30096376F91B382 FOREIGN KEY (listes_id) REFERENCES liste (id)');
         $this->addSql('ALTER TABLE magasin_article ADD CONSTRAINT FK_7585D73A20096AE3 FOREIGN KEY (magasin_id) REFERENCES magasin (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE magasin_article ADD CONSTRAINT FK_7585D73A7294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE utilisateur_liste ADD CONSTRAINT FK_3A37722AFB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE utilisateur_liste ADD CONSTRAINT FK_3A37722AE85441D8 FOREIGN KEY (liste_id) REFERENCES liste (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
@@ -46,16 +43,13 @@ final class Version20250304152636 extends AbstractMigration
         $this->addSql('ALTER TABLE liste_article DROP FOREIGN KEY FK_B30096376F91B382');
         $this->addSql('ALTER TABLE magasin_article DROP FOREIGN KEY FK_7585D73A20096AE3');
         $this->addSql('ALTER TABLE magasin_article DROP FOREIGN KEY FK_7585D73A7294869C');
-        $this->addSql('ALTER TABLE utilisateur_liste DROP FOREIGN KEY FK_3A37722AFB88E14F');
-        $this->addSql('ALTER TABLE utilisateur_liste DROP FOREIGN KEY FK_3A37722AE85441D8');
         $this->addSql('DROP TABLE article');
         $this->addSql('DROP TABLE categorie');
         $this->addSql('DROP TABLE liste');
         $this->addSql('DROP TABLE liste_article');
         $this->addSql('DROP TABLE magasin');
         $this->addSql('DROP TABLE magasin_article');
-        $this->addSql('DROP TABLE utilisateur');
-        $this->addSql('DROP TABLE utilisateur_liste');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
--- src/Controller/StatisticController.php
+++ src/Controller/StatisticController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class AccueilController extends AbstractController
+final class StatisticController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/statistic', name: 'app_statistic')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+        return $this->render('statistic/index.html.twig', [
+            'controller_name' => 'StatisticController',
         ]);
     }
 }
