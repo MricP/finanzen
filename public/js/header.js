b058db0f4d25572f6c0b510d0115d20e8c47a39b
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2025-03-11 12:58:48 +0100

    feat(): footer, et navbar menu burger

--- public/js/header.js
+++ public/js/header.js
@@ -4,14 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const openButton = document.querySelector(".btn-profile-user");
     const closeButton = document.querySelector(".close-btn");
     const profilePopup = document.getElementById("profilePopup");
+    const menuBurger = document.querySelector(".menu-burger");
+    const headerColumnLinks = document.querySelector(".header-column-links");
+    let isOpen = false;
 
-    if (openButton && closeButton && profilePopup) {
+    if (openButton && closeButton && profilePopup && menuBurger) {
         openButton.addEventListener('click', () => {
+            console.log("vvvved")
             profilePopup.classList.add("visible");
         });
 
         closeButton.addEventListener('click', () => {
             profilePopup.classList.remove("visible");
+        });
+
+        menuBurger.addEventListener('click', () => {
+            isOpen = !isOpen;
+            if (isOpen) {
+                headerColumnLinks.classList.add("visible");
+            } else {
+                headerColumnLinks.classList.remove("visible");
+            }
         });
     } else {
         console.error("Un ou plusieurs éléments n'ont pas été trouvés dans le DOM.");
